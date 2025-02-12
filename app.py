8169e7d4b868f6f5ae54552e39d419fd713bd339
eniompw <71254710+eniompw@users.noreply.github.com>
2025-02-12 13:53:14 +0000

    signup page

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
