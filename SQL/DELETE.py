1654767b088c1f027016431e2b0844f148bfdfff
eniompw <71254710+eniompw@users.noreply.github.com>
2025-03-07 12:44:14 +0000

    SQL

---
+++ SQL/DELETE.py
@@ -0,0 +1,7 @@
+import sqlite3
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+cur.execute("""	DELETE FROM Employee
+                WHERE EmpID = 1122
+            """)
+con.commit()
---
+++ SQL/INSERT.py
@@ -0,0 +1,7 @@
+import sqlite3
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+cur.execute("""	INSERT INTO Employee (EmpID, EmpName, HireDate, Salary)
+                VALUES (1122, 'Bloggs', '#1/1/2001#', 18000)
+            """)
+con.commit()
